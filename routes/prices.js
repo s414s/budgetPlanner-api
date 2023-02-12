ab19deb9542670c41ecb2cdc81fce73f45dbcf73
Alberto Salas <alsalasher@gmail.com>
2023-02-12 11:18:05 +0100

    remove not needed endpoints

--- routes/prices.js
+++ routes/prices.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {authToken, authAdmin} = require('../config/authtoken');
+const {authToken} = require('../config/authtoken');
 const {wrapper} = require('../config/wrapper');
 const CtrPrices = require('../controllers/prices');
 
-router.get("/list/:id_budget", [authToken], wrapper(CtrPrices.listPrices, "get list prices"))
-router.get("/get/:id_price", [authToken], wrapper(CtrPrices.getPrice, "get price"))
 router.post("/add", [authToken], wrapper(CtrPrices.addPrice, "add price"))
 router.put("/update/:id_price", [authToken], wrapper(CtrPrices.updatePrice, "update price"))
 router.delete("/del/:id_price", [authToken], wrapper(CtrPrices.deletePrice, "delete price"))
